masking neurons fix with training data

mammoth/datasets/seq_cifar10.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 from datasets.seq_tinyimagenet import base_path
 from PIL import Image
 import numpy as np
-from datasets.utils.validation import get_train_val
-from datasets.utils.continual_dataset import ContinualDataset, store_masked_loaders, store_clip_masked_loaders
+from datasets.utils.validation import get_train_val, get_train_data_clip
+from datasets.utils.continual_dataset import ContinualDataset, store_masked_loaders, store_clip_masked_loaders, store_xai_masked_loaders
 from datasets.utils.continual_dataset import get_previous_train_loader
 from typing import Tuple
 from datasets.transforms.denormalization import DeNormalize
@@ -74,6 +74,14 @@ class SequentialCIFAR10(ContinualDataset):
                                   (0.2470, 0.2435, 0.2615))
              ]
     )
+    TRANSFORM_XAI = transforms.Compose(
+            [transforms.Resize(32),
+             transforms.CenterCrop(32),
+             transforms.ToTensor(),
+             transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                  (0.2470, 0.2435, 0.2615))
+             ]
+    )
 
     def get_data_loaders(self):
         transform = self.TRANSFORM
@@ -88,21 +96,32 @@ class SequentialCIFAR10(ContinualDataset):
                                   download=True, transform=transform)
         if self.args.validation:
             train_dataset, test_dataset = get_train_val(train_dataset,
-                                                    test_transform, self.NAME)
+                                                    transform, self.NAME)
         else:
             test_dataset = CIFAR10(base_path() + 'CIFAR10', train=False,
                                    download=True, transform=test_transform)
 
         train, test = store_masked_loaders(train_dataset, test_dataset, self)
+
         return train, test
 
     def get_clip_data_loaders(self):
-        test_transform = self.TRANSFORM_CLIP
-        test_dataset = CIFAR10(base_path() + 'CIFAR10', train=False,
-                               download=True, transform=test_transform)
+        transform_clip = self.TRANSFORM_CLIP
+        transform_xai = self.TRANSFORM_XAI
 
-        test = store_clip_masked_loaders(test_dataset, self)
-        return
+        train_dataset_clip = MyCIFAR10(base_path() + 'CIFAR10', train=True,
+                                  download=True, transform=transform_clip)
+        train_val_dataset_clip, test_val_dataset = get_train_data_clip(train_dataset_clip, transform_clip)
+
+        train_dataset_xai = MyCIFAR10(base_path() + 'CIFAR10', train=True,
+                                  download=True, transform=transform_xai)
+        train_val_dataset_xai, test_val_dataset = get_train_data_clip(train_dataset_xai, transform_xai)
+
+        print(self.i)
+        val_train_clip = store_clip_masked_loaders(train_val_dataset_clip, self)
+        val_train_xai = store_xai_masked_loaders(train_val_dataset_xai, self)
+
+        return val_train_clip, val_train_xai
 
     def not_aug_dataloader(self, batch_size):
         transform = transforms.Compose([transforms.ToTensor(), self.get_normalization_transform()])
@@ -112,6 +131,9 @@ class SequentialCIFAR10(ContinualDataset):
         train_loader = get_previous_train_loader(train_dataset, batch_size, self)
 
         return train_loader
+
+    def increment_class(self):
+        self.i += self.N_CLASSES_PER_TASK
 
     @staticmethod
     def get_transform():
@@ -139,4 +161,6 @@ class SequentialCIFAR10(ContinualDataset):
         transform = DeNormalize((0.4914, 0.4822, 0.4465),
                                 (0.2470, 0.2435, 0.2615))
         return transform
+
+
 

mammoth/datasets/utils/continual_dataset.py
@@ -34,6 +34,7 @@ class ContinualDataset:
         self.train_loader = None
         self.test_loaders = []
         self.test_clip_loaders = []
+        self.test_xai_loaders = []
 
         self.i = 0
         self.args = args
@@ -127,12 +128,12 @@ def store_masked_loaders(train_dataset: datasets, test_dataset: datasets,
     setting.test_loaders.append(test_loader)
     setting.train_loader = train_loader
 
-    setting.i += setting.N_CLASSES_PER_TASK
+    # setting.i += setting.N_CLASSES_PER_TASK
+
     return train_loader, test_loader
 
 
-def store_clip_masked_loaders(test_dataset: datasets,
-                    setting: ContinualDataset) -> Tuple[DataLoader, DataLoader]:
+def store_clip_masked_loaders(val_train_dataset: datasets, setting: ContinualDataset) -> Tuple[DataLoader, DataLoader]:
     """
     Divides the dataset into tasks.
     :param train_dataset: train dataset
@@ -140,17 +141,40 @@ def store_clip_masked_loaders(test_dataset: datasets,
     :param setting: continual learning setting
     :return: train and test loaders
     """
-    test_mask = np.logical_and(np.array(test_dataset.targets) >= setting.i,
-        np.array(test_dataset.targets) < setting.i + setting.N_CLASSES_PER_TASK)
+    val_mask = np.logical_and(np.array(val_train_dataset.targets) >= setting.i,
+        np.array(val_train_dataset.targets) < setting.i + setting.N_CLASSES_PER_TASK)
 
-    test_dataset.data = test_dataset.data[test_mask]
-    test_dataset.targets = np.array(test_dataset.targets)[test_mask]
+    val_train_dataset.data = val_train_dataset.data[val_mask]
+    val_train_dataset.targets = np.array(val_train_dataset.targets)[val_mask]
 
-    test_loader = DataLoader(test_dataset,
-                             batch_size=setting.args.batch_size, shuffle=False, num_workers=4)
-    setting.test_clip_loaders.append(test_loader)
+    train_loader = DataLoader(val_train_dataset,
+                              batch_size=setting.args.batch_size, shuffle=True, num_workers=4, drop_last=DROP_LAST)
 
-    return test_loader
+    setting.val_train_clip_loader = train_loader
+
+    return train_loader
+
+def store_xai_masked_loaders(val_train_dataset: datasets, setting: ContinualDataset) -> Tuple[DataLoader, DataLoader]:
+    """
+    Divides the dataset into tasks.
+    :param train_dataset: train dataset
+    :param test_dataset: test dataset
+    :param setting: continual learning setting
+    :return: train and test loaders
+    """
+    val_mask = np.logical_and(np.array(val_train_dataset.targets) >= setting.i,
+        np.array(val_train_dataset.targets) < setting.i + setting.N_CLASSES_PER_TASK)
+
+    val_train_dataset.data = val_train_dataset.data[val_mask]
+    val_train_dataset.targets = np.array(val_train_dataset.targets)[val_mask]
+
+    train_loader = DataLoader(val_train_dataset,
+                              batch_size=setting.args.batch_size, shuffle=True, num_workers=4, drop_last=DROP_LAST)
+
+    setting.val_train_xai_loader = train_loader
+
+    return train_loader
+
 
 def get_previous_train_loader(train_dataset: datasets, batch_size: int,
                               setting: ContinualDataset) -> DataLoader:

mammoth/utils/clip_utils.py
@@ -153,13 +153,10 @@ def get_clip_image_features(model, dataset, device = "cuda"):
     all_features= []
 
     with torch.no_grad():
-        for k, test_loader in enumerate(dataset.test_clip_loaders):
-            if k < len(dataset.test_clip_loaders) - 1:
-                continue
-            for data in tqdm(test_loader):
-                images, labels = data
-                features = model.encode_image(images.to(device))
-                all_features.append(features)
+        for k, data in enumerate(dataset.val_train_clip_loader):
+            images, _, _= data
+            features = model.encode_image(images.to(device))
+            all_features.append(features)
     img_features = torch.cat(all_features)
     return img_features
 
@@ -173,12 +170,12 @@ def get_target_activations(target_model, dataset, target_layers = ["layer4"], de
         hooks[target_layer] = eval(command)
 
     with torch.no_grad():
-        for k, test_loader in enumerate(dataset.test_loaders):
-            if k < len(dataset.test_loaders) - 1:
-                continue
-            for data in tqdm(test_loader):
-                images, labels = data
-                features = target_model(images.to(device))
+        # for k, train_loader in enumerate(dataset.train_xai_loader):
+            # if k < len(dataset.train_loader) - 1:
+            #     continue
+        for k, data in enumerate(dataset.val_train_xai_loader):
+            images, _, _ = data
+            features = target_model(images.to(device))
     
     for target_layer in target_layers:
         target_features = torch.cat(all_features[target_layer])